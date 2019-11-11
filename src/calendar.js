4a7b29f189a47015f93b2931f0b6fec8f26eb1cb
Utente <utente@MacBook-Pro-di-Utente.local>
2019-11-11 16:09:55 +0100

    add webpack dev server config

---
+++ src/calendar.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: "Calendar",
+    version: "0.1"
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Calendar = require("./calendar.js")
+// ES6+
+// import Calendar from "./calendar.js"
+
+console.log('WEBPACK 1', Calendar)
