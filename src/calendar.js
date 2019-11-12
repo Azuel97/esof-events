46d4538b207c064a13de1364b69a4f94982b40f9
Utente <utente@MacBook-Pro-di-Utente.local>
2019-11-12 15:51:30 +0100

    add calendar module

--- src/calendar.js
+++ src/calendar.js
@@ -1,8 +1,29 @@
+const Data = require("./data.js")
+const CalendarData = Data.calendar
 const calendarData = document.querySelector("#calendar-data")
 
 
 module.exports = {
     name: "Calendar",
     version: "0.1",
-    dom: calendarData
+    render: function(){
+        console.log("---- INIT CALENDAR ----")
+        calendarData.innerHTML = `
+            ${CalendarData.map(item => `
+                <ion-item-group>
+                    <ion-item-divider sticky>
+                        <ion-label>${item.time}</ion-label>
+                    </ion-item-divider>
+                ${item.session.map(session => `
+                    <ion-item>
+                        <ion-label>
+                            <h3>${session.nome}</h3>
+                            <p>${session.inizio}&mdash; ${session.fine} - ${session.luogo}</p>
+                        </ion-label>
+                    </ion-item>
+                `).join('\n')}
+                </ion-item-group>
+            `).join('\n')}
+        `
+    }
 }
